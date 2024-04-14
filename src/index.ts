8e57a150619283b0c9e7d69274e0f659ad747d0d
devgoncalo <goncalopintoneto@gmail.com>
2024-04-14 13:31:44 +0100

    feat: redis rate limiter class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,51 @@
-import { Hono } from "hono";
-import { todos } from "./data/data.json"
+import { Context, Env, Hono } from "hono";
+import { BlankInput } from "hono/types";
+import { env } from "hono/adapter";
+
+import { todos } from "./data/data.json";
+
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis/cloudflare";
 
 const app = new Hono();
+
+const cache = new Map();
+
+class RedisRateLimiter {
+  static instance: Ratelimit;
+
+  static getInstance(c: Context<Env, "/todos/:id", BlankInput>) {
+    if (!this.instance) {
+      const { REDIS_URL, REDIS_TOKEN } = env<{
+        REDIS_URL: string;
+        REDIS_TOKEN: string;
+      }>(c);
+
+      const redisClient = new Redis({
+        token: REDIS_TOKEN,
+        url: REDIS_URL,
+      });
+
+      const ratelimit = new Ratelimit({
+        redis: redisClient,
+        limiter: Ratelimit.slidingWindow(10, "10 s"),
+        ephemeralCache: cache,
+      });
+
+      this.instance = ratelimit;
+      return this.instance;
+    } else {
+      return this.instance;
+    }
+  }
+}
 
 app.get("/todos/:id", (c) => {
   const todoId = c.req.param("id");
   const todoIndex = Number(todoId);
   const todo = todos[todoIndex] || {};
 
-  return c.json({todo})
-})
+  return c.json({ todo });
+});
 
-export default app
+export default app;
