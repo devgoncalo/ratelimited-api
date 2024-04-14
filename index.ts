e8fc385562ffed646f4d706e11f816a083570b30
devgoncalo <goncalopintoneto@gmail.com>
2024-04-14 13:21:10 +0100

    feat: upstash redis database setup and data

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/todos", (c) => {
-  return c.json({message: "Hello World!"})
-})
-
-export default app
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono";
+import { todos } from "./data/data.json"
+
+const app = new Hono();
+
+app.get("/todos/:id", (c) => {
+  const todoId = c.req.param("id");
+  const todoIndex = Number(todoId);
+  const todo = todos[todoIndex] || {};
+
+  return c.json({todo})
+})
+
+export default app
